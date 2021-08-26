4b4f91ccd6ad469bbff5a2e4372fa10f9f9e0d75
Jose M Zelezarof <zelezarof@gmail.com>
2021-08-26 09:43:46 -0400

    Fixing warnings Fixing parameters assignation

--- scripts/broken_bits.pl
+++ scripts/broken_bits.pl
@@ -1,13 +1,15 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
 
 use Socket;
 
 # 192.168.23.13 is in one of the subnets listed in $subnetlist
 # There are four errors in this script.  Locate and fix the errors.
 
-$dotted_address   = q|192.168.23.13|;
+my $dotted_address   = q|192.168.101.255|;
 
-$subnetlist       = [
+my $subnetlist       = [
                '192.168.1.0/22',
                '192.168.10.0/24',
                '192.168.20.0/22',
@@ -22,16 +24,16 @@ if(subnets_have_host($subnetlist, $dotted_address)) {
 }
 
 sub subnets_have_host {
-   my($subnets, $dotted_addr)  = @args;
+   my($subnets, $dotted_addr)  = @_;
    my $stat    = undef;
 
    printf("Starting checks for %s...\n", $dotted_addr);
 
    my $address    = unpack('N', inet_aton($dotted_addr));
 
-   foreach $subnet_w_bits (@{$subnets}) {
+   foreach my $subnet_w_bits (@{$subnets}) {
       my($subnet_dots, $bitmask)   = split(/\//, $subnet_w_bits);
-      $subnet           = unpack('N', inet_aton($subnet_dots));
+      my $subnet           = unpack('N', inet_aton($subnet_dots));
       my $wildcard      = (0xFFFFFFFF >> $bitmask);
       my $netmask       = (0xFFFFFFFF >> $bitmask) ^ 0xFFFFFFFF;
       my $broadcast     = $subnet | ($netmask | 0xFFFFFFFF);
