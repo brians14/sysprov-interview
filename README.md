57602d971cf3835daec437951636ba12cd4bff33
Jose M Zelezarof <zelezarof@gmail.com>
2021-08-26 13:37:56 -0400

    Importing initial terraform configuration Documenting steps required

--- README.md
+++ README.md
@@ -12,3 +12,64 @@ Small exercise to assess interview candidates approach to development with IaC, 
 3. Convert the [perl script](scripts/broken_bits.pl) to another language of choice (python, ruby, etc.)
 
 4. (Preferably Azure/GCP) Using terraform, implement a solution using Linux VM instance(s) running the web server configuration from exercise 1, that is resilient to downtime/outages.
+
+
+------------------------------------------------------------------------------
+* Dependency installation  (Fedora 34)
+  * [Terraform](https://www.terraform.io/downloads.html)
+    ```bash
+    # Download the zip file; x64 for this example
+    curl -o ~/Downloads/terraform.zip  https://releases.hashicorp.com/terraform/1.0.5/terraform_1.0.5_linux_amd64.zip
+    
+    # Unzip the archive and place it in a directory that is part of your system's PATH
+    $ echo $PATH
+    $ unzip ~/Downloads/terraform.zip -d ~/.local/bin/
+    
+    # Ensure the binary is accessible
+    $ which terraform
+    ```
+  * [Azure CLI](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=dnf)
+    ```bash
+    # Import repository key
+    $ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    
+    # Create repository file
+    echo -e "[azure-cli]
+    name=Azure CLI
+    baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+    enabled=1
+    gpgcheck=1
+    gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo
+    
+    # Login to your Azure account
+    $ az login 
+    
+    # Create a Service Principal
+    $ az ad sp create-for-rbac --name sysprov_interview  #TODO Save the output from the previous command in a secure place
+   
+    # Set Principal credentials in your local environment
+    echo '
+    export ARM_SUBSCRIPTION_ID="<azure_subscription_id>"
+    export ARM_TENANT_ID="<azure_subscription_tenant_id>"
+    export ARM_CLIENT_ID="<service_principal_appid>"
+    export ARM_CLIENT_SECRET="<service_principal_password>"
+    ' >> ~/.bashrc
+    
+    # Load the new variables by executing the .bashrc script
+    source ~/.bashrc
+    
+    # Initialize terraform project
+    terraform init
+    
+    # Create a changes plan
+    terraform plan -out main.tfplan
+    
+    # Apply changes
+    terraform apply main.tfplan
+    
+    # Destroy plan to destroy infrastructure
+    terraform plan -destroy -out main.destroy.tfplan
+    
+    # Destroy infrastructure
+    terraform apply main.destroy.tfplan
+    ```
---
+++ scripts/main.tf
@@ -0,0 +1,173 @@
+terraform {
+    required_version = ">=0.12"
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "~>2.0"
+        }
+    }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "amdsysprov" {
+    name = "amdSysProvGroup"
+    location = "canadacentral"
+
+    tags = {
+        environment = "AMD SYSPROV"
+    }
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "amdsysprov" {
+    name                = "amdSysProvNet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "canadacentral"
+    resource_group_name = azurerm_resource_group.amdsysprov.name
+
+    tags = {
+        environment = "AMD SYSPROV"
+    }
+}
+
+# Create subnet
+resource "azurerm_subnet" "amdsysprov" {
+    name                 = "amdSysProvSubnet"
+    resource_group_name  = azurerm_resource_group.amdsysprov.name
+    virtual_network_name = azurerm_virtual_network.amdsysprov.name
+    address_prefixes       = ["10.0.1.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "amdsysprov" {
+    name                         = "amdSysProvPublicIP"
+    location                     = "canadacentral"
+    resource_group_name          = azurerm_resource_group.amdsysprov.name
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = "AMD SYSPROV"
+    }
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "amdsysprov" {
+    name                = "amdSysProvNetworkSecurityGroup"
+    location            = "canadacentral"
+    resource_group_name = azurerm_resource_group.amdsysprov.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "AMD SYSPROV"
+    }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "amdsysprov" {
+    name                      = "amdSysProvNIC"
+    location                  = "canadacentral"
+    resource_group_name       = azurerm_resource_group.amdsysprov.name
+
+    ip_configuration {
+        name                          = "myNicConfiguration"
+        subnet_id                     = azurerm_subnet.amdsysprov.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.amdsysprov.id
+    }
+
+    tags = {
+        environment = "AMD SYSPROV"
+    }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "secint" {
+    network_interface_id      = azurerm_network_interface.amdsysprov.id
+    network_security_group_id = azurerm_network_security_group.amdsysprov.id
+}
+
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+    keepers = {
+        resource_group = azurerm_resource_group.amdsysprov.name
+    }
+
+    byte_length = 8
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "amdsysprov" {
+    name                        = "diag${random_id.randomId.hex}"
+    resource_group_name         = azurerm_resource_group.amdsysprov.name
+    location                    = "canadacentral"
+    account_tier                = "Standard"
+    account_replication_type    = "LRS"
+
+    tags = {
+        environment = "AMD SYSPROV"
+    }
+}
+
+# # Create (and display) an SSH key
+# resource "tls_private_key" "apache_ssh" {
+#   algorithm = "RSA"
+#   rsa_bits = 4096
+# }
+
+# output "tls_private_key" {
+#     value = tls_private_key.apache_ssh.private_key_pem
+#     sensitive = true
+# }
+
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "amdsysprov" {
+    name                  = "amdSysProvVM"
+    location              = "canadacentral"
+    resource_group_name   = azurerm_resource_group.amdsysprov.name
+    network_interface_ids = [azurerm_network_interface.amdsysprov.id]
+    size                  = "Standard_B1s"
+
+    os_disk {
+        name              = "apache_hd"
+        caching           = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    source_image_reference {
+        publisher = "OpenLogic"
+        offer     = "CentOS"
+        sku       = "7.5"
+        version   = "latest"
+    }
+
+    computer_name  = "web"
+    admin_username = "zelezarof"
+    disable_password_authentication = true
+
+    admin_ssh_key {
+        username       = "zelezarof"
+        public_key     = file("~/.ssh/id_rsa.pub")
+    }
+
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.amdsysprov.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "AMD SYSPROV"
+    }
+}
